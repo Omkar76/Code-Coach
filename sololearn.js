5c072f7bcc76a5e6daa4c007f3f4b28d03731f4b
Omkar <56870381+Omkar76@users.noreply.github.com>
2019-12-22 11:09:16 +0530

    sololearn.js
    Javscript code.

---
+++ sololearn.js
@@ -0,0 +1,360 @@
+"use strict";
+
+const proGroup=[
+
+    {id:9726746,name:'Diego'},
+    
+    {id:6850261,name:'Seb TheS'},
+
+    {id:10654452,name:'Javier Felipe Toribio'},
+
+    {id:11595192,name:'Saboor Hakimi'},
+
+    {id:4826331,name:'Stark'},
+
+    {id:2200299,name:'Prometheus'},
+    
+    {id:7429689,name:'Denis Robberg'},
+    
+    {id:14057407,name:'Doriav isakov'},
+    
+    {id:5371585,name:'Sick Line Bro'},
+    
+    {id:12731601,name:'Man Of Action'},
+    
+    {id:10631738,name:'Onyx'},
+    
+    {id:7038378,name:'Arb Rahim Badsa'},
+    
+    {id:5351922,name:'Orion'},
+    
+    {id:5097984,name:'Mahesh Mahato'},
+    
+    {id:6029224,name:'Cepagrave'},
+    
+    {id:14045357,name:'dinku789 Mehta'},
+    
+    {id:15314637,name:'M J'},
+    
+    {id:3800510,name:'Dennis'},
+    
+    {id:11694334,name:'Danik Davletshin'},
+    
+    {id:10314085,name:'🇮🇳Omkar🕉'}, /*1*/
+    
+    {id:917611,name:'Dmitry Ilin'},
+    
+    {id:12691463,name:'Marko Krstic'},
+    
+    {id:11932555,name:'Coder Kitten'},
+    
+    {id:16738670,name:'Danilo Cintrao'},
+    
+    {id:9223786,name:'HonFu'},
+    
+    {id:1981140,name:'bahha'},
+    
+    {id:13714142,name:'r1c5'},
+    
+    {id:14558754,name:'César Eduardo Aguas Aldana'},
+    
+    {id:9172889,name:'John Bryan Pit Acaso'},
+    
+    {id:4446335,name:'Pawan Maurya'},
+    
+    {id:8994942,name:'Alice'},
+    
+    {id:2472699,name:'Saurabh Tiwari'},
+    
+    {id:4819943,name:'Gamma001'},
+    
+    {id:748195,name:'Janusz bujak'},
+    
+    {id:13253423,name:'CarrieForle'},
+    
+    {id:16537801,name:'XXX'},
+    
+    {id:5680106,name:'Stanislav'},
+    
+    {id:16761972,name:'molang'},
+    
+    {id:14447570,name:'Kevin Star'},
+    
+    {id:11186837,name:'Alpha5and5'},/*2*/
+
+    {id:4703173,name:'Igor Kostrikin'},
+    
+    {id:8909178,name:'Julio Codesal'},
+    
+    {id:4735163,name:'Mohit The Coder (M.S.D)'},
+    
+    {id:12875805,name:'MCGA'},
+    
+    {id:5327650,name:'LukArToDo'},
+    
+    {id:15315513,name:'Preity'},
+    
+    {id:15747130,name:'Manfrid Strengell'},
+    
+    {id:13314481,name:'Alexandr'},
+    
+    {id:10807810,name:'Gyorgi Theisz'},
+    
+    {id:8159873,name:'Jax'},
+    
+    {id:12361701,name:'OfficialAz3'},
+    
+    {id:2520889,name:'Michal'},
+    
+    {id:12687006,name:'Ankur Dingh Oli'},
+    
+    {id:12586353,name:'Arbokcharm'},
+    
+    {id:2271086,name:'Param Siddharth'},
+    
+    {id:9479351,name:'Priya'},
+    
+    {id:5044306,name:'Shadow'},
+    
+    {id:2464021,name:'med gazzeh'},
+    
+    {id:26195909,name:'Edword'},
+    
+    {id:2670745,name:'Fabian blaz'},/*3*/
+    
+    {id:10959671,name:'Manito'},
+    
+    {id:16667392,name:'Kiril'},
+    
+    {id:14810203,name:'Michal Straka'},
+    
+    {id:12451288,name:'Partha Sarathi'},
+    
+    {id:13943183,name:'lvl 1 crook'},
+    
+    {id:1189743,name:'Jean Michel Furtado M\'Peko'},
+    
+    {id:1502544,name:'Davy hermans'},
+    
+    {id:10000369,name:'Matthew G'},
+    
+    {id:12140080,name:'yessine'},
+    
+    {id:9751950,name:'Juan Pablo Vila'},
+    
+    {id:15687328,name:'Pavle'},
+    
+    {id:15771326,name:'Vinay_GB'},
+    
+    {id:1416592,name:'MorrisMoss'},
+    
+    {id:12942084,name:'Aakanksha💕'},
+    
+    {id:6965381,name:'Christian Stallberg'},
+    
+    {id:5743989,name:'Mart yaziki'},
+    
+    {id:10902790,name:'Rajesh Singh Rathore'},
+    
+    {id:4825354,name:'Louis'},
+    
+    {id:4776735,name:'Sam'},
+    
+    {id:9710664,name:'Michail Getmanskiy'},
+    /*4*/
+    
+    {id:15574926,name:'Thomas'},
+    
+    {id:3426759,name:'Oma Falk'},
+    
+    {id:2671088,name:'Barracoder'},
+    
+    {id:13608843,name:'RadaR'},
+    
+    {id:4469476,name:'Demahrix'},
+    
+    {id:16685145,name:'Laura Stasiulė'},
+    
+    {id:16465769,name:'Michele'},
+    
+    {id:14159513,name:'Linda Schnabel'},
+    
+    {id:15551308,name:'Steve'},
+    
+    {id:12514810,name:'Julia'},
+    
+    {id:5597395,name:'Princetronixx'},
+    
+    {id:2443327,name:'nueutrino-4RD'},
+    
+    {id:16768297,name:'Mihai Apostol'},
+    
+    {id:6980352,name:'Александр Петроченко '},
+    
+    {id:15122805,name:'yasir mulla'},
+    
+    {id:8507344,name:'🌶️anssens'},
+    
+    {id:5040440,name:'Daljeet Singh'},
+    
+    {id:16130260,name:'Gabriel Altwarg-Peel'},
+    
+    {id:16390036,name:'Antek Olszevski'},
+    
+    {id:5355537,name:'Mehmet Süzer'},/*5*/
+    
+    /*biggest Array of my life 😁*/
+    
+   
+];
+
+
+/*I haven't uploaded yet*/
+const platforms=[
+    {
+     name:'Codepen',
+     link:'',
+ logo:'https://blog.codepen.io/wp-content/uploads/2012/06/Button-White-Large.png',
+    },
+    
+    {
+     name:'Github',
+     link:'',
+     logo:'https://cdn2.iconfinder.com/data/icons/black-white-social-media/64/social_media_logo_github-512.png'
+    },
+    {
+        name:'Sololearn',
+        link:'https://code.sololearn.com/cxvdJ5tLoFUr/?ref=app',
+        logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0vd4Yv3GzTd1SrOMuJB8KOtVqYce1Oa3-YZIv022lIayy7sds'
+    }
+    
+];
+/*sort alphabetically , case insensitive , and ascending . not needed anymore
+
+proGroup.sort((a,b)=> a.name.toUpperCase()<b.name.toUpperCase()?-1:1);
+*/
+
+/*invoked when input given to searchbar*/
+
+let sortByName=name=>{
+
+    for(let user of proGroup){
+    
+        if(!user.name.toLowerCase().
+        includes(name.toLowerCase())) {
+            user.card.style.display='none';
+         }
+        else {
+            user.card.style.display='flex';
+        }
+    }
+}
+
+
+
+let setCards=()=>{
+
+    let main=document.getElementById('main');
+
+  for(let user of proGroup)
+   {    
+
+        let card=document.
+        createElement('div');
+
+    
+        let avt=new Image('100px','100px'); 
+        avt.onerror=()=>{
+        avt.onerror=null;       avt.src="https://www.sololearn.com/Images/NoAvatar.jpg";
+        }
+        avt.src="https://api.sololearn.com/Uploads/Avatars/"+user.id+".jpg";
+
+        let link=document.createElement('a');
+        link.innerHTML=user.name;
+        link.setAttribute('href',                "https://www.sololearn.com/Profile/"+user.id+"/?ref=app");
+
+
+        card.classList.add('card');
+        avt.classList.add('avtar');
+        link.classList.add('anch');
+        link.style['padding-left']='20px';
+        
+        
+        card.appendChild(avt);
+        card.appendChild(link);
+        main.appendChild(card);
+        
+        user.card=card; /*new property of user that store reference to div (card) for that user to later manipulate it while searching
+
+        users are elements of proGroup array
+        */
+   }
+
+   document.querySelector('main').
+   style.opacity=1;
+
+}
+
+
+/*default parameter 0*/
+
+let setCount=(n=0)=>{
+    document.getElementById('count').
+    innerText=n;
+    if(n<100)setTimeout(setCount,20,++n);    
+
+}
+
+const greetings="Congratulations to all winners of code coach challenge!";
+
+let sayCongo=(n=0)=>{
+
+  let  msgBox=document.
+  getElementById('congoMessage');
+
+  msgBox.innerText+=greetings[n];
+
+  if(n<greetings.length-1)
+    setTimeout(sayCongo,70,++n);
+  else
+    document.getElementsByTagName('svg')[0].
+    style.opacity=1;
+
+}
+
+let setFooter=()=>{
+    let foot=document.
+    querySelector('footer');
+    
+    for(const code of platforms){
+        let a=document.
+        createElement('a');
+        
+        a.setAttribute('href',code.link);
+        a.style.margin='auto';
+        
+        let logo=new Image();
+        logo.src=code.logo;
+        logo.setAttribute('alt',code.name);
+        logo.classList.add('logo');
+        
+        a.appendChild(logo);
+        foot.appendChild(a);
+    }
+    
+}
+
+window.onload=()=>{
+
+    setCards();
+
+    setCount();
+
+    sayCongo();
+    
+    setFooter();
+    
+    //console.log(proGroup.length);
+    /*images set through html made dom loading slow , and I was facing problems in starting js at very beginning of code. so all done with js😴*/
+    
+}
